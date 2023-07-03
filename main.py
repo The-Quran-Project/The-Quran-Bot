a9773eb6d5e17545acf0068cc8da05f51f8296c7
Nusab19 <nusabtaha33@gmail.com>
2023-07-04 03:05:41 +0600

    KeepAlive

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+from datetime import datetime
+
+import httpx, time
+import multiprocessing
+
 from bot import runBot
+
+
+
+
+renderUrl = "https://quran-bot-2i9n.onrender.com/"
+def check():
+    while 1:
+        try:
+            r = httpx.get(renderUrl, timeout=30, follow_redirects=1).status_code
+        except:r=404
+        print(f"\nStatus {r} at: {datetime.now()}\n")
+        time.sleep(13)
+
+
+multiprocessing.Process(target=check).start()
 
 if __name__ == "__main__":
     runBot()
