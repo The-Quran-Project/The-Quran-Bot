8b7afeeb1d2e8c6f6bc218afd4c22638013a74b7
Nusab Taha <nusabtaha33@gmail.com>
2024-02-16 23:05:07 +0600

    fix typo in admin commands

--- bot/handlers/adminCommand.py
+++ bot/handlers/adminCommand.py
@@ -9,28 +9,25 @@ def escapeHtml(text: str):
     return html.escape(str(text))
 
 
-buttons = [
-    [
-        InlineKeyboardButton("Users Len", callback_data="admin users len"),
-        InlineKeyboardButton("Get Users", callback_data="admin users all"),
-    ],
-    [
-        InlineKeyboardButton("Chats Len", callback_data="admin chats len"),
-        InlineKeyboardButton("Get Chats", callback_data="admin chats all"),
-    ],
-    [
-        InlineKeyboardButton("Admins Len", callback_data="admin admins len"),
-        InlineKeyboardButton("Get Admins", callback_data="admin admins all"),
-    ],
-]
-
-
 async def adminCommand(u: Update, c):
     """Sends a message to the user on how to use the bot"""
     message = u.effective_message
     userID = u.effective_user.id
     user = db.getUser(userID)
-
+    buttons = [
+        [
+            InlineKeyboardButton("Users Len", callback_data="admin users len"),
+            InlineKeyboardButton("Get Users", callback_data="admin users all"),
+        ],
+        [
+            InlineKeyboardButton("Chats Len", callback_data="admin chats len"),
+            InlineKeyboardButton("Get Chats", callback_data="admin chats all"),
+        ],
+        [
+            InlineKeyboardButton("Admins Len", callback_data="admin admins len"),
+            InlineKeyboardButton("Get Admins", callback_data="admin admins all"),
+        ],
+    ]
     if not user.get("is_admin"):
         await message.reply_html("<b>You are not an admin</b>")
         return
@@ -43,7 +40,9 @@ async def adminCommand(u: Update, c):
 /getUser <code>chatID</code> [ Reply to a message ]
 """
 
-    await message.reply_html(reply, reply_markup=InlineKeyboardButton(buttons), quote=True)
+    await message.reply_html(
+        reply, reply_markup=InlineKeyboardMarkup(buttons), quote=True
+    )
 
 
 async def forwardMessage(u: Update, c):
