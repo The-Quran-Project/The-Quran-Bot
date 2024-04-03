bdb530ba3f7280b789e533f1c21484a660fcaa0c
Nusab Taha <nusabtaha33@gmail.com>
2024-04-03 15:09:06 +0600

    Add LocalDB class and scheduled tasks

--- bot/handlers/database.py
+++ bot/handlers/database.py
@@ -1,5 +1,8 @@
 import os
-
+import time
+import asyncio
+import schedule
+import threading
 
 from dotenv import load_dotenv
 from pymongo.server_api import ServerApi
@@ -17,11 +20,60 @@ if os.environ.get("LOCAL"):
     dns.resolver.default_resolver.nameservers = ["8.8.8.8"]
 
 
+class LocalDB:
+    """Local Database to store users and chats in memory."""
+
+    def __init__(self, users: list, chats: list) -> None:
+        self.users = list(users)
+        self.chats = list(chats)
+        self.admins = [i["_id"] for i in self.users if i.get("admin")]
+        print(f"LocalDB: {len(self.users)} users, {len(self.chats)} chats")
+
+    def findUser(self, userID: int) -> dict:
+        return next((i for i in self.users if i["_id"] == userID), None)
+
+    def findChat(self, chatID: int) -> dict:
+        return next((i for i in self.chats if i["_id"] == chatID), None)
+
+    def addUser(self, user) -> None:
+        self.users.append(user)
+        return None
+
+    def addChat(self, chat) -> None:
+        self.chats.append(chat)
+        return None
+
+    def updateUser(self, userID: int, settings: dict) -> dict:
+        user = self.findUser(userID)
+        if not user:
+            user = self.addUser(userID)
+
+        user["settings"] = {**user["settings"], **settings}
+        return user
+
+    def updateChat(self, chatID: int, settings: dict) -> dict:
+        chat = self.findChat(chatID)
+        if not chat:
+            chat = self.addChat(chatID)
+
+        chat["settings"] = {**chat["settings"], **settings}
+        return chat
+
+    def getAllUsers(self) -> list:
+        return self.users
+
+    def getAllChat(self) -> list:
+        return self.chats
+
+    def getAllAdmins(self) -> list:
+        return self.admins
+
+
 class Database:
     def __init__(self) -> None:
         uri = os.environ.get("MONGODB_URI")
         self.client = MongoClient(uri, server_api=ServerApi("1"))
-        self.db = self.client.quranbot
+        self.db = self.client.development
         self.defaultSettings = {
             "font": 1,  # 1 -> Uthmani, 2 -> Simple
             "showTafsir": True,
@@ -36,31 +88,65 @@ class Database:
             "previewLink": False,  # Show preview of the Tafsir link
             "restrictedLangs": ["ar"],
         }
-        self.admins = [i["_id"] for i in self.db.users.find({"is_admin": True})]
+        # --- Local DB ---
+        self.queue = []
+        users = self.db.users.find({})
+        chats = self.db.chats.find({})
+        self.localDB = LocalDB(users, chats)
+
+        # --- Scheduled Tasks ---
+        schedule.every(20).seconds.do(self.runQueue)
+
+        def runScheduledTasks():
+            while True:
+                try:
+                    schedule.run_pending()
+                except Exception as e:
+                    print("Error in scheduled tasks:", e)
+                time.sleep(1)
+
+        threading.Thread(target=runScheduledTasks).start()
+
+    @property
+    def admins(self):
+        return self.localDB.getAllAdmins()
 
     def getAllUsers(self):
-        return [i for i in self.db.users.find({})]
+        res = self.localDB.getAllUsers()
+        return res
 
     def getAllChat(self):
-        return [i for i in self.db.chats.find({})]
+        res = self.localDB.getAllChat()
+        return res
 
     def getAllAdmins(self):
-        return [i for i in self.admins]
+        res = self.localDB.getAllAdmins()
+        return res
 
     def getUser(self, userID: int):
-        return self.db.users.find_one({"_id": userID})
+        res = self.localDB.findUser(userID)
+        return res
 
     def getChat(self, chatID: int):
-        return self.db.chats.find_one({"_id": chatID})
+        res = self.localDB.findChat(chatID)
+        return res
 
     def addUser(self, userID: int):
         user = {"_id": userID, "settings": self.defaultSettings}
-        self.db.users.insert_one(user)
+        self.localDB.addUser(user)
+
+        func = self.db.users.insert_one
+        value = user
+        self.queue.append((func, value))
         return user
 
     def addChat(self, chatID: int):
         chat = {"_id": chatID, "settings": self.defaultGroupSettings}
-        self.db.chats.insert_one(chat)
+        self.localDB.addChat(chat)
+
+        func = self.db.chats.insert_one
+        value = chat
+        self.queue.append((func, value))
         return chat
 
     def updateUser(self, userID: int, settings: dict):
@@ -69,9 +155,11 @@ class Database:
             user = self.addUser(userID)
 
         settings = {**user["settings"], **settings}
-
-        self.db.users.update_one({"_id": userID}, {"$set": {"settings": settings}})
-        return None  # self.getUser(userID)
+        self.localDB.updateUser(userID, settings)
+        func = self.db.users.update_one
+        value = ({"_id": userID}, {"$set": {"settings": settings}})
+        self.queue.append((func, value))
+        return None
 
     def updateChat(self, chatID: int, settings: dict):
         chat = self.getChat(chatID)
@@ -79,12 +167,27 @@ class Database:
             chat = self.addChat(chatID)
 
         settings = {**chat["settings"], **settings}
-
-        # TODO : Do it in a better way
         settings["restrictedLangs"] = list(set(settings["restrictedLangs"]))
-        self.db.chats.update_one({"_id": chatID}, {"$set": {"settings": settings}})
+        self.localDB.updateChat(chatID, settings)
 
-        return None  # self.getChat(chatID)
+        func = self.db.chats.update_one
+        value = ({"_id": chatID}, {"$set": {"settings": settings}})
+        self.queue.append((func, value))
+        return None
+
+    def runQueue(self):
+        print("--- Running Queue ---")
+        start = time.time()
+
+        for func, value in self.queue:
+            if isinstance(value, tuple):
+                func(*value)
+            else:
+                func(value)
+
+        end = time.time()
+        self.queue = []
+        print(f"Time taken: {end - start:.2f}s")
 
     # TODO: keep count of all the requests handled per day
     # Run this in a separate thread or use a TypeHandler to run after the other handlers (group=3)
@@ -107,13 +210,13 @@ class Database:
 db = Database()
 
 
-def main():
-    #users = db.getAllUsers()
+async def main():
+    users = db.getAllUsers()
     chats = db.getAllChat()
-    #print("Total Users:", len(users))
+    print("Total Users:", len(users))
     print("Total Chats:", len(chats))
     print(db.getAllAdmins())
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
