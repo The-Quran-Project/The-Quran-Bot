261be59ed1379de73b3ff61366e0304ede8e4c51
Nusab Taha <nusabtaha33@gmail.com>
2024-04-22 17:39:16 +0600

    Refactor decorators to use onlyGroupAdmin instead of developersOrAdmins

--- bot/handlers/helpers/decorators.py
+++ bot/handlers/helpers/decorators.py
@@ -8,61 +8,71 @@ from ..database import db
 developers = db.admins
 
 
-def onlyDeveloper(func: Callable):
-    @wraps(func)
-    async def wrapper(u: Update, c, *a, **k):
-        if u.channel_post:
+def onlyDeveloper(notifyNonAdminUsers: bool = True):
+    """
+    Decorator to check if the user is a developer
+
+    Parameters:
+        notifyNonAdminUsers (bool): Whether to notify non-admin users that they are not authorized to use the command
+
+    Returns:
+        wrapper (Callable): The wrapper function
+    """
+
+    def outerWrapper(func: Callable):
+        @wraps(func)
+        async def wrapper(u: Update, c, *a, **k):
+            if u.channel_post:
+                return await func(u, c, *a, **k)
+
+            if u.effective_user.id not in developers:
+                if not notifyNonAdminUsers:
+                    return
+                return await u.effective_message.reply_html(
+                    "<b>You are not authorized to use this command</b>"
+                )
             return await func(u, c, *a, **k)
 
-        if u.effective_user.id not in developers:
-            return await u.effective_message.reply_html(
-                "<b>You are not authorized to use this command</b>"
-            )
-        return await func(u, c, *a, **k)
+        return wrapper
 
-    return wrapper
+    return outerWrapper
 
 
-def onlyGroupAdmin(func: Callable):
-    @wraps(func)
-    async def wrapper(u: Update, c, *a, **k):
-        if u.effective_chat.type not in ("group", "supergroup"):
+def onlyGroupAdmin(allowDev: bool = False, notifyNonAdminUsers: bool = True):
+    """
+    Decorator to check if the user is an admin of the group
+
+    Parameters:
+        allowDev (bool): Whether to allow developers to use the command
+        notifyNonAdminUsers (bool): Whether to notify non-admin users that they are not authorized to use the command
+
+    Returns:
+        wrapper (Callable): The wrapper function
+    """
+
+    def outerWrapper(func: Callable):
+        @wraps(func)
+        async def wrapper(u: Update, c, *a, **k):
+            if allowDev and u.effective_user.id in developers:
+                return await func(u, c, *a, **k)
+
+            if u.effective_chat.type not in ("group", "supergroup"):
+                return await func(u, c, *a, **k)
+
+            userID = u.effective_user.id
+
+            if userID == 1087968824:  # Group Anonymous Bot
+                return await func(u, c, *a, **k)
+
+            admins = [i.user.id for i in await u.effective_chat.get_administrators()]
+            if userID not in admins:
+                if not notifyNonAdminUsers:
+                    return
+                return await u.effective_message.reply_html(
+                    "<b>You must be an admin of the group to use this command</b>"
+                )
             return await func(u, c, *a, **k)
 
-        userID = u.effective_user.id
+        return wrapper
 
-        if userID == 1087968824:  # Group Anonymous Bot
-            return await func(u, c, *a, **k)
-
-        admins = [i.user.id for i in await u.effective_chat.get_administrators()]
-        if userID not in admins:
-            return await u.effective_message.reply_html(
-                "<b>You must be an admin of the group to use this command</b>"
-            )
-        return await func(u, c, *a, **k)
-
-    return wrapper
-
-
-def developersOrAdmins(func: Callable):
-    @wraps(func)
-    async def wrapper(u: Update, c, *a, **k):
-        if u.effective_user.id in developers:
-            return await func(u, c, *a, **k)
-
-        if u.effective_chat.type not in ("group", "supergroup"):
-            return await func(u, c, *a, **k)
-
-        userID = u.effective_user.id
-
-        if userID == 1087968824:  # Group Anonymous Bot
-            return await func(u, c, *a, **k)
-
-        admins = [i.user.id for i in await u.effective_chat.get_administrators()]
-        if userID not in admins:
-            return await u.effective_message.reply_html(
-                "<b>You must be an admin of the group to use this command</b>"
-            )
-        return await func(u, c, *a, **k)
-
-    return wrapper
+    return outerWrapper
